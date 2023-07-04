c7931317e34a47765696781e15965389076b1812
Ayush <77936340+Ayush-Rawat-02@users.noreply.github.com>
2023-07-04 15:48:55 +0530

    Update sample_data.js

--- sample_data.js
+++ sample_data.js
@@ -1,36 +1,36 @@
 const sample_data = [
   {
-    name: "Ayush",
+    name: "Harry",
     age: 20,
     location: "Dehradun",
     job_role: "Full-Stack Web developer",
   },
   {
-    name: "Aman",
+    name: "Jarico",
     age: 21,
     location: "Haldwani",
     job_role: "Data Scientist",
   },
   {
-    name: "Akshay",
+    name: "Lensko",
     age: 19,
     location: "Lucknow",
     job_role: "DevOps Engineer",
   },
   {
-    name: "Nikhil",
+    name: "Chris",
     age: 19,
     location: "Ghaziabad",
     job_role: "Software Tester",
   },
   {
-    name: "Shishank",
+    name: "Shushant",
     age: 21,
     location: "Haldwani",
     job_role: "Software Developer",
   },
   {
-    name: "Harshit",
+    name: "Vanze",
     age: 20,
     location: "Gurgaon",
     job_role: "Data Engineer",
